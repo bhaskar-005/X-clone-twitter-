406f40bc6577df21671358e3088f1eae5fe0ed2b
bhaskar <bhandari222ji@gmail.com>
2024-04-29 14:12:56 +0530

    updated redis

--- x-client/graphql/api.ts
+++ x-client/graphql/api.ts
@@ -1,7 +1,7 @@
 import {GraphQLClient} from 'graphql-request'
 
 const isClient = typeof window !== 'undefined' && window
-export const graphqlClient = new GraphQLClient(process.env.GQL_URL! ,{
+export const graphqlClient = new GraphQLClient('https://x-clone-twitter.onrender.com/graphql' ,{
     headers:{
         __token: isClient? `Bearer ${localStorage.getItem('__token')}`:''
     }
--- x-server/src/redis/index.ts
+++ x-server/src/redis/index.ts
@@ -1,5 +1,7 @@
 import Redis from "ioredis"
 
 export const redisClient = new Redis(process.env.REDIS_URL!,{
-    maxRetriesPerRequest:500
+    maxRetriesPerRequest:500,
+    connectTimeout: 10000,
+    lazyConnect:true,
 });
